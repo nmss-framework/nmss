76f359c21d6a6117e50edfa467018d6bca764896
rlucha <rlucha@gmail.com>
2015-02-28 02:00:25 +0100

    better preview

--- doc/js/doc.js
+++ doc/js/doc.js
@@ -40,11 +40,17 @@ var nmss = (function() {
 })(elements);
 
 var elements = {
-    button: {
+    'button': {
         template: '<button class="button">This is a button</button>'
     },
-    input: {
+    'input': {
         template: '<input type="text" class="input" placeholder="input">'
+    },
+    'grid': {
+        template: '<div class="g"></div>'
+    },
+    'grid-item': {
+        template: '<div class="g-u"></div>'
     }
 };
 
