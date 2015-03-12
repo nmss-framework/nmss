c209e481dbc01539d86178cf71304971e0dbda3f
rlucha <roberto@sportmaniacs.com>
2015-03-12 14:44:36 +0100

    better utils

--- doc/js/doc.js
+++ doc/js/doc.js
@@ -1,46 +1,7 @@
-var elements = require('./elements.js');
-
-module.exports = function(elements) {
-    'use strict';
-
-    var state = {
-        cElement: undefined
-    };
-
-    function dragStart(e) {
-        console.log('dragStart');
-        console.log(e);
-        e.dataTransfer.setData('element', e.target.id);
-    }
-
-    function dragEnter(e) {
-        console.log('dragEnter');
-        e.preventDefault();
-        return true;
-    }
-
-    function dragOver(e) {
-        console.log('dragOver');
-        e.preventDefault();
-    }
-
-    function dragDrop(e) {
-        console.log('dragDrop');
-        var elementType = e.dataTransfer.getData('element');
-        e.target.value += elements[elementType].template;
-        e.stopPropagation();
-        return false;
-    }
-
-    return {
-        dragStart: dragStart,
-        dragEnter: dragEnter,
-        dragOver: dragOver,
-        dragDrop: dragDrop
-    };
-};
-
 document.addEventListener("DOMContentLoaded", function(event) {
+
+    Drag = require('./drag.js');
+    window.drag = new Drag();
 
     var emmet = require('./vendor/emmet.min.js');
     var rest = require('rest');
