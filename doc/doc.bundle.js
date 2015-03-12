05fa5dc7793aa06ab0007258ab5e3de42d077a41
rlucha <roberto@sportmaniacs.com>
2015-03-12 13:43:58 +0100

    pause

--- doc/doc.bundle.js
+++ doc/doc.bundle.js
@@ -46,6 +46,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var emmet = require('./vendor/emmet.min.js');
     var rest = require('rest');
 
+    var pause = false;
+
     var sectionCode = document.querySelector('#section-code');
     var previewer = document.querySelector('#section-preview');
 
@@ -59,20 +61,31 @@ document.addEventListener("DOMContentLoaded", function(event) {
         use_tab: true // expand abbreviations by Tab key
     });
 
+    window.addEventListener("keydown", keyDownTextField, false);
+
+    function keyDownTextField (e) {
+    var keyCode = e.keyCode;
+        if (keyCode === 80) {
+            pause = pause? false : true;
+        }
+    }
+
+
     setInterval(rePaint, 1000);
 
     function rePaint(e) {
-
-        previewer.contentWindow.document.open();
-        previewer.contentWindow.document.write(
-            '<!DOCTYPE html>' +
-            '<html><head><title>My dynamic document</title>' +
-            '<link rel="stylesheet" href="css/nmss.css?">' +
-            '<link rel="stylesheet" href="css/doc.css">' +
-            '</head>' +
-            '<body class="wrapper-preview"><div class="content-preview">' + sectionCode.value.replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&nbsp;/g, '') +
-            '</div></body></html>');
-        previewer.contentWindow.document.close();
+        if(!pause) {
+            previewer.contentWindow.document.open();
+            previewer.contentWindow.document.write(
+                '<!DOCTYPE html>' +
+                '<html><head><title>My dynamic document</title>' +
+                '<link rel="stylesheet" href="css/nmss.css?">' +
+                '<link rel="stylesheet" href="css/doc.css">' +
+                '</head>' +
+                '<body class="wrapper-preview"><div class="content-preview">' + sectionCode.value.replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&nbsp;/g, '') +
+                '</div></body></html>');
+            previewer.contentWindow.document.close();
+        }
     }
 });
 },{"./elements.js":2,"./vendor/emmet.min.js":3,"rest":6}],2:[function(require,module,exports){
